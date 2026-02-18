95e7f996b58ebd545f67f1747a39bda53206fc3f
Narayan Paul <158485405+narayan123git@users.noreply.github.com>
2026-02-18 12:39:08 +0530

    Restrict CORS to specific origin for security

--- api/genai.js
+++ api/genai.js
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import aboutMe from "./aboutMe.json" assert { type: "json" };
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://narayanpaulportfolio.netlify.app");
   res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
